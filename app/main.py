68dafe6d023681cac7e87ff7669071815c26218b
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2025-01-22 09:58:34 +0000

    chore: updated logs service

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from app.routes import users
+from app.routes import logs
 from fastapi import FastAPI
 from app import  models
 from app.core import database
@@ -9,5 +9,5 @@ app = FastAPI()
 # Create tables if they do not exist (usually run this once at the beginning)
 models.Base.metadata.create_all(bind=database.engine)
 
-app.include_router(users.router,prefix="/api/v1/users", tags=["Users"])
+app.include_router(logs.router, prefix="/api/v1/logs", tags=["Users"])
 
